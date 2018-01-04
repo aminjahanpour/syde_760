f15885f631508c55e78c6b7fca8e530f05505d9e
amin <amin.jahanpour@uwaterloo.ca>
2018-01-04 13:04:55 -0500

    a

--- test.py
+++ test.py
@@ -9,6 +9,12 @@ s = 1.
 f1 = lambda x: x - 0.5 * x ** 2.
 f2 = lambda x: (s-x) - (s-x) ** 2.
 # print(f1(x))
-plt.plot(x, f1(x))
+plt.plot(x, f1(x),label='f1')
+plt.plot(x, f2(x),label='f2')
+plt.plot(x, f1(x) + f2(x),label='f1+f2')
+
+plt.scatter(x=[2./3.], y=[2./3.], label='optimum')
+
+plt.legend()
 
 plt.show()
