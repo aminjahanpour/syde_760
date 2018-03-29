ab5f0bd474c4ddf7acc1672e78c0a6de5c3f23ec
aminjahanpour <amin.jahanpour.gmail>
2018-03-29 16:47:36 -0400

    yo2

--- cov_.py
+++ cov_.py
@@ -1,14 +1,16 @@
+import math
+
+import matplotlib.pyplot as plt
 import numpy as np
 
 
-def Q(u):
+def Q_fun(u):
     return (1.0 / u[2]) * u[0] * pow(u[1], (2. / 3.)) * pow(u[3], 0.5)
 
 
 no_nodes = 4
 
 mu = [10, 1.15, 0.012, 0.0009]
-cv = [1.] * no_nodes
 
 ro = np.ones(shape=[no_nodes, no_nodes])
 
@@ -16,6 +18,8 @@ ro[0] = [1, 0.6, 0.1, 0]
 ro[1] = [0.6, 1, 0.15, 0]
 ro[2] = [0.1, 0.15, 1, 0]
 ro[3] = [0, 0, 0, 1]
+
+cv = [0.1] * no_nodes
 
 c = np.zeros(shape=[no_nodes, no_nodes], dtype=float)
 for i in range(no_nodes):  # rows
@@ -26,8 +30,22 @@ N = 1000000
 
 u = np.random.multivariate_normal(mu, c.tolist(), N)
 
-Q_ = list(map(Q, u))
+Q_ = list(map(Q_fun, u))
+Q_ = [value for value in Q_ if not math.isnan(value)]
+Q = np.array(Q_)
+
+print(np.mean(Q))
+print(np.var(Q))
+print(np.std(Q))
 
 print("Q >= 25", len([x for x in Q_ if x >= 25.]) / N)
 print("Q >= 31", len([x for x in Q_ if x >= 31.]) / N)
 print("Q >= 50", len([x for x in Q_ if x >= 50.]) / N)
+
+# rr=range=np.arange(min(Q),max(Q))
+plt.hist(Q, bins=100, label="Cv=%f" % cv[0], fc=(0, 0, 1, 0.5))
+
+plt.xlabel("Q")
+plt.ylabel("Frequency")
+plt.legend()
+plt.show()
--- matrix.py
+++ matrix.py
@@ -1,7 +1,61 @@
+import random
+
 import numpy as np
+from numpy import linalg as LA
 
 A = np.array([[3,1],
               [1,2]])
 B = np.array([9,8])
 x = np.linalg.solve(A, B)
-print(x)
+# print(x)
+
+
+# p1= np.array(
+#     [[0.9, 0.1],
+#      [0.2, 0.8]]
+#             )
+
+p1 = np.array(
+    [[0.1, 0.6, 0.3],
+     [0.2, 0.2, 0.6],
+     [0.4, 0.4, 0.2]])
+
+p2 = LA.matrix_power(p1, 2)
+
+p_steady = LA.matrix_power(p1, 2000)
+
+print(p1)
+print(p2)
+print(p_steady)
+
+s = "B"
+for i in range(100):
+    print(s)
+
+    p_ = random.random()
+
+    if s == "B":
+        if p_ <= p1[0][0]:
+            ns = "B"
+        elif p1[0][0] < p_ <= p1[0][0] + p1[0][1]:
+            ns = "S"
+        elif p1[0][0] + p1[0][1] < p_:
+            ns = "H"
+
+    elif s == "S":
+        if p_ <= p1[1][0]:
+            ns = "B"
+        elif p1[1][0] < p_ <= p1[1][0] + p1[1][1]:
+            ns = "S"
+        elif p1[1][0] + p1[1][1] < p_:
+            ns = "H"
+
+    elif s == "H":
+        if p_ <= p1[2][0]:
+            ns = "B"
+        elif p1[2][0] < p_ <= p1[2][0] + p1[2][1]:
+            ns = "S"
+        elif p1[2][0] + p1[2][1] < p_:
+            ns = "H"
+
+    s = ns
