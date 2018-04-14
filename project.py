de06b40fb8cf446aa4402680f2565740fdfed64e
aminjahanpour <amin.jahanpour@gmail.com>
2018-04-14 17:31:40 -0400

    h

--- project.py
+++ project.py
@@ -24,10 +24,40 @@ import numpy as np
 from scipy.optimize import linprog
 
 
-def lp_solve(s_init, inflow):
+def lp_solve(t, cv, s_init, inflow):
 
-    #    ut, sf1,  su1,  sf2, su2, sf3,  su3
-    c = [-2, 0.25, 0.25, 0.5, 0.5, 0.25, 0.25]
+
+    # t = 1
+    # cv = 0.5
+
+    if t == 1:          # mean = 6
+        if cv == 0.5:   # sd = 3
+            inflow_set = [1.5, 6, 12]
+            #    ut, sf1,    su1,    sf2,    su2,    sf3,    su3
+            c = [-2, 0.1359, 0.1359, 0.6826, 0.6826, 0.1573, 0.1573]
+        if cv == 1:     # sd = 6
+            inflow_set = [6, 15, 18]
+            c = [-2, 0.6827, 0.6827, 0.1359, 0.1359, 0.0214, 0.0214]
+
+
+    elif t == 2:          # mean = 9
+        if cv == 0.5:   # sd = 4.5
+            inflow_set = [2.25, 9, 18]
+            #    ut, sf1,    su1,    sf2,    su2,    sf3,    su3
+            c = [-2, 0.1359, 0.1359, 0.6827, 0.6827, 0.1573, 0.1573]
+        if cv == 1:     # sd = 9
+            inflow_set = [9, 22.5, 31.5]
+            c = [-2, 0.6827, 0.6827, 0.1359, 0.1359, 0.0228, 0.0228]
+
+
+    elif t == 3:          # mean = 7
+        if cv == 0.5:   # sd = 3.5
+            inflow_set = [1.75, 7, 14]
+            #    ut, sf1,    su1,    sf2,    su2,    sf3,    su3
+            c = [-2, 0.1359, 0.1359, 0.6827, 0.6827, 0.1587, 0.1587]
+        if cv == 1:     # sd = 7
+            inflow_set = [7, 17.5, 24.5]
+            c = [-2, 0.6827, 0.6827, 0.1359, 0.1359, 0.0228, 0.0228]
 
 
     """
@@ -63,9 +93,9 @@ def lp_solve(s_init, inflow):
         [0, 0, 0, 0, 0, 1, -1]
         ]
 
-    b_eq = [5 - inflow,
-            6 - inflow,
-            7 - inflow
+    b_eq = [inflow_set[0] - inflow,
+            inflow_set[1] - inflow,
+            inflow_set[2] - inflow
             ]
 
     no_bounds = (-100, 100)
@@ -84,7 +114,7 @@ I1 = 5
 S_1 = 2
 I_1 = 6
 
-u_1 = lp_solve(s_init=S_1, inflow=I_1)
+u_1 = lp_solve(t=1, cv=0.5, s_init=S_1, inflow=I_1)
 print("t=1", S_1, I_1, u_1)
 
 """
@@ -93,7 +123,7 @@ t=2
 S_2 = S_1 + I_1 - u_1
 I_2 = 9
 
-u_2 = lp_solve(s_init=S_2, inflow=I_2)
+u_2 = lp_solve(t=2, cv=0.5, s_init=S_2, inflow=I_2)
 print("t=2", S_2, I_2, u_2)
 
 """
@@ -102,5 +132,13 @@ t=3
 S_3 = S_2 + I_2 - u_2
 I_3 = 7
 
-u_3 = lp_solve(s_init=S_3, inflow=I_3)
+u_3 = lp_solve(t=3, cv=0.5, s_init=S_3, inflow=I_3)
 print("t=3", S_3, I_3, u_3)
+
+
+"""
+f(x)=∑(t=1:3){2u_t+s_t}
+"""
+
+f = 2 * (u_1+u_2+u_3) + (S_1+S_2+S_3)
+print(f)
