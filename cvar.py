d571989b8c996c9ca5a09e9a32eafd51741f9d85
aminjahanpour <amin.jahanpour@gmail.com>
2018-06-01 17:19:06 -0400

    finalizing

--- cvar.py
+++ cvar.py
@@ -31,22 +31,35 @@ import cma
 import numpy as np
 
 
-def cvar_obj(u):
-    inflows = np.array(
-        [np.random.normal(6, 6 * cv, N), np.random.normal(9, 9 * cv, N), np.random.normal(7, 7 * cv, N)]).T
+def cvar_obj(u, simu=False):
+    inflow = np.random.normal(6, 6 * cv, N)
+    inflow_1 = np.array([x if x >= 0 else 0 for x in inflow])
+
+    inflow = np.random.normal(9, 9 * cv, N)
+    inflow_2 = np.array([x if x >= 0 else 0 for x in inflow])
+
+    inflow = np.random.normal(7, 7 * cv, N)
+    inflow_3 = np.array([x if x >= 0 else 0 for x in inflow])
+
+    inflows = np.array([inflow_1, inflow_2, inflow_3]).T
+
     f = []
+
     for I in inflows:
-        S = [2, 0, 0, 0]
+
+        S = [0, 0, 0, 0]
 
         S[0] = 2
         S[1] = S[0] + I[0] - u[0]  # S[1]
         S[2] = S[1] + I[1] - u[1]  # S[2]
         S[3] = S[2] + I[2] - u[2]  # S[3]
 
-        if all([0 < x < 6 for x in S]):
+        if all([0 <= x <= 6 for x in S]):
             f.append(2 * (u[0] + u[1] + u[2]) + (S[1] + S[2] + S[3]))
 
     f.sort()
+
+    # print(len(f))
 
     min_f = min(f)
     max_f = max(f)
@@ -61,47 +74,35 @@ def cvar_obj(u):
 
     cvar_a = np.mean([x for x in f if x <= var_a])
 
-    # return var_a, cvar_a, f
-    return cvar_a
+    if simu:
+        return var_a, cvar_a, f
+    else:
+        return cvar_a
 
 
-
-if __name__ == '__main__':
-    N = 10000
-    cv = 0.5
-    alfa = 0.1
-    budget = 10000
-
+def optimization():
     initial_solution = np.array([3, 3, 3])
-
-    bnds = ((0, 6), (0, 6), (0, 6))
 
     es = cma.CMAEvolutionStrategy(initial_solution, 0.5, {'bounds': [0, 6]})
     ans = es.optimize(cvar_obj, maxfun=budget)
 
-    print("_____________________")
-    print(ans.best.f)
-    print([round(xx, 4) for xx in ans.best.x])
+    return [round(xx, 4) for xx in ans.best.x], ans.best.f
 
+    # bnds = ((0, 6), (0, 6), (0, 6))
     # print(['#']*50)
     #
     # ans = NelderMeadSimplexSearch.minimize(cvar_obj, initial_solution, max_iterations=budget, bounds=bnds)
     # print(ans)
-
-    #
-    #
     # ans2 = minimize(cvar_obj, initial_solution,  bounds=bnds,
     #                 options={'maxiter': budget, 'disp': True})
     # print(ans2)
 
-    """
-    simulation
-    """
 
-    # cvar_a = cvar_obj([2, 2, 2] , alfa, cv, N)
+def simulation(dv):
+    var_a, cvar_a, f = cvar_obj(dv, simu=True)
 
-
-    # print("alfa=%6.3f,   var_a=%6.3f,   cvar_a=%6.3f" % (alfa, var_a, cvar_a, ))
+    print("alfa=%6.3f,   var_a=%6.3f,   cvar_a=%6.3f,   f: len:%i  mean:%6.3f  std:%6.3f  " % (
+    alfa, var_a, cvar_a, len(f), np.mean(f), np.std(f)))
 
     # fig = plt.figure()
     # fig.suptitle('bold figure suptitle', fontsize=14, fontweight='bold')
@@ -124,6 +125,21 @@ if __name__ == '__main__':
     # plt.hist(f)
     # plt.scatter([var_a, cvar_a], [0, 0], c='r')
     #
-    # # plt.xlabel("")
-    # # plt.ylabel("")
+    # plt.xlabel("")
+    # plt.ylabel("")
     # plt.show()
+
+if __name__ == '__main__':
+    N = 100000
+    cv = 0.5
+    alfa = 0.2
+    budget = 100
+
+    for alpha in [0.5]:
+        dv_opt, f_opt = optimization()
+
+        print("opt_____________________")
+        print(dv_opt, f_opt)
+        print()
+
+        simulation(dv_opt)
